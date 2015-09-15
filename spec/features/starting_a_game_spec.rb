4b76eda0613bf6e088025aaaadec60f0d0a5f4f4
chn.challenger <chn.challenger@gmail.com>
2015-09-15 16:27:35 +0100

    progress

--- spec/features/starting_a_game_spec.rb
+++ spec/features/starting_a_game_spec.rb
@@ -6,4 +6,12 @@ feature 'Starting a new game' do
     click_link 'New Game'
     expect(page).to have_content "What's your name?"
   end
+
+  scenario 'When I submit my name' do
+    visit '/'
+    click_link 'New Game'
+    #enter name in form
+    #click submit
+    expect(page).to have_content "Test"
+  end
 end
