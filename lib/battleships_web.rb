54e2ba6e3b1cd434607e81f7f597fa9ba768d91c
Lucy Fang <lucyfang2@gmail.com>
2015-09-16 19:07:06 +0100

    one player game

--- lib/battleships_web.rb
+++ lib/battleships_web.rb
@@ -4,24 +4,19 @@ require_relative 'ship'
 require_relative 'cell'
 require_relative 'board'
 
-
 class BattleshipsWeb < Sinatra::Base
 
   enable :sessions
 
-
   set :views, proc { File.join(root, '..', 'views') }
 
-  get '/' do
-    erb :index
-  end
-
   get '/new_game' do
-    @name = params[:name]
     erb :new_game
   end
 
   get '/new_board' do
+    session[:name] = params[:name]
+    @name = session[:name]
     session[:board] = Board.new(Cell) if session[:board] == nil
     @display = session[:board].print_board
     session[:size] = 0
@@ -55,39 +50,12 @@ class BattleshipsWeb < Sinatra::Base
         @message = "Number of ships sunk: #{@sunk_ships}"
       end
     end
-    @message = "You won!" if @sunk_ships == 5
-    erb :fire
+    if @sunk_ships == 5
+      erb :winner
+    else
+      erb :fire
+    end
   end
-
-
-
-
-
-  get '/example_board' do
-    board = Board.new(Cell)
-    ship1 = Ship.new(2)
-    ship2 = Ship.new(3)
-    ship3 = Ship.new(4)
-    board.place(ship1, :A1)
-    board.place(ship2, :C2)
-    board.place(ship3, :E4)
-
-    # board.shoot_at(:A1)
-    # board.shoot_at(:E4)
-    # board.shoot_at(:A7)
-    # board.shoot_at(:E9)
-    # board.shoot_at(:C3)
-    # board.shoot_at(:D8)
-    # board.shoot_at(:C9)
-    # board.shoot_at(:A2)
-    # board.shoot_at(:F7)
-    # board.shoot_at(:G9)
-    @result = board.print_board
-    # @result = board.opponent_board
-
-    erb :example_board
-  end
-
 
   # start the server if ruby file executed directly
   run! if app_file == $0
